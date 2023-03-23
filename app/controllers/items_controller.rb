6e36beacbb1dd3356b1563a5478a04f9511c7e2c
osteen <osteen.mutinda@student.moringaschool.com>
2023-03-23 20:25:42 +0300

    Complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,9 @@
 class ItemsController < ApplicationController
+
+    def index
+        items = Item.all
+        render json: items.as_json(include: { user: { only: [:id, :username, :city] } })
+      end
+      
+
 end
